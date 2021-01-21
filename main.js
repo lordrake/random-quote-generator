72ca544f08b2815e24723e6910abab31045e8cdf
lordrake <danilocolella17@gmail.com>
2021-01-21 17:12:38 +0100

    Aggiungo animazione e autore

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 console.log('js collegato');
 
+const spinner = document.querySelector('#js-spinner');
 
 async function getQuote() {
-  console.log('Button clicked, Getting a quote..');
+
+  // remove the "hidden" class on the spinner
+  spinner.classList.remove('hidden');
+  // disable the quote button
+  newQuoteButton.disabled = true;
 
   const endpoint = 'https://api.quotable.io/random';
 
@@ -14,14 +19,28 @@ async function getQuote() {
     }
 
     const jsonResponse = await response.json();
-    console.log(jsonResponse.content);
+    console.log(jsonResponse);
+    displayQuote(jsonResponse.content, jsonResponse.author);
 
   } catch (error) {
     console.log(error);
     alert('Failed to fetch new quote');
 
+  } finally {
+    // enable the quote button
+    newQuoteButton.disabled = false;
+    // add the "hidden" class back again
+    spinner.classList.add('hidden');
   }
 
+}
+
+function displayQuote(quote, author) {
+  const quoteText = document.querySelector('#js-quote-text');
+  const quoteAuthor = document.querySelector('#js-quote-author');
+
+  quoteText.textContent = quote;
+  quoteAuthor.textContent = author;
 }
 
 
